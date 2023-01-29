58fc358ee5e78d8a9052243b781e601bbccabf97
Kevin Hahn <hahn.kev@gmail.com>
2023-01-29 10:24:01 +0700

    add client code gen

--- __tests__/integration/index.ts
+++ __tests__/integration/index.ts
@@ -1,5 +1,8 @@
+import { DocumentType, graphql } from "src/gql";
 import request, { Response } from "supertest";
 import app from "../../src/webserver";
+import { TypedDocumentNode } from '@graphql-typed-document-node/core';
+import { HelloQuery } from "src/gql/graphql";
 
 const expectSuccessfulGraphQLResponse = (response: Response) => {
   expect(response.body.errors).toBeUndefined();
@@ -7,11 +10,15 @@ const expectSuccessfulGraphQLResponse = (response: Response) => {
 
 describe("Integration testing GraphQL API", () => {
   it("return 'hello GraphQL world' for hello query", async () => {
-    const helloQuery = /* GraphQL */ `
+    const helloQuery = /* GraphQL */ graphql(`
       query hello {
         hello
       }
-    `;
+    `);
+
+    //if your query had parameters the type would also be infered here.
+    const autoTypedResult = executeQuery(helloQuery, {});
+    (await autoTypedResult).hello;
 
     const createHelloQuery = () => {
       return {
@@ -28,14 +35,39 @@ describe("Integration testing GraphQL API", () => {
 
     expectSuccessfulGraphQLResponse(response);
 
-    // TODO: How can I use '@graphql-codegen/x' here to automatically create the
-    // correct typeing for my helloQuery?
+    
     type HelloQueryResult = {
-      data: {
-        hello: string;
-      };
+      data:  DocumentType<typeof helloQuery>;
     };
+    type HelloQueryResult2 = {
+      data: HelloQuery
+    }
     const result = response.body as HelloQueryResult;
+    const result2 = response.body as HelloQueryResult2;
     expect(result.data.hello).toBe("hello GraphQL world");
   });
 });
+
+
+async function executeQuery<
+  TQueryDocument extends TypedDocumentNode<any, any>, 
+  TResponseType = DocumentType<TQueryDocument>, 
+  TVariables = TQueryDocument extends TypedDocumentNode< any, infer TType>  ? TType  : never
+>(query: TQueryDocument, variables: TVariables): Promise<TResponseType> {
+
+  const createHelloQuery = () => {
+    return {
+      query: query,
+      variables: variables,
+    };
+  };
+
+  const response = await request(app)
+    .post("/graphql")
+    .set("Content-Type", "application/json")
+    .set("Accept", "application/json")
+    .send(createHelloQuery());
+
+  expectSuccessfulGraphQLResponse(response);
+  return response.body as TResponseType
+}
